dfd1106d9850d469be5e9cbce62ff9ba369f9ccd
TurnerIvey <47543078+TurnerIvey@users.noreply.github.com>
2022-09-01 14:39:14 -0500

    Create September1st.py

---
+++ September1st.py
@@ -0,0 +1,99 @@
+print("""Todays date is 9/1/2022, and these are nots on Starting System
+
+
+Warm-up is some video about "potential future of robots and robot/human interaction related to the workforce."
+
+
+-- MENU --
+''''''''''
+
+Please choose on of the options below, and press enter.
+
+
+1) System Changeover
+2) Implementation Methods
+3) related to Data Migration
+4) phases in Data Migration
+5) issues in Data Migration
+6) related to Business M&A includes Merger vs Acquisitio and System Integration follow M&A
+7) find examples and todays assignment. 
+
+
+Yo YO YO Mr. WHIIITEEEEE, type a number with corresponding topics.""")
+
+answer = "empty"
+
+while (answer == "empty"):
+  userchoice = input()
+  if(userchoice == "1"):
+    print("A system changeover is the process of transitioning from one system to another. This can be a major or minor change, and can involve changes to personnel, processes, or technology. The process used to switch from one system to another. An individual changing their own personal system, or that of their family, often won't have a lot of thought or concern around it. However, the larger the environment, the more problematic and complicated a system changeover may be. A company or organization likely handles and stores people's personal data, financial information, and may also be tied to people's livelihood, health and wellness, or a region's infrastructure.")
+    
+    print("Similar to how SBISD changed to Canvas instead of itslearning. There is often a lot of backlash, and a lot more trainning hours for the employed. I reckon that they ought to find out if its worst setting up or not. This brings us toooooo Differing implementation")
+    
+  elif(userchoice == "2"):
+    print("Differing implementation methods have differing trade-offs and risks, the 'best method' is always situationally dependent. The implementation methods are as follows:")
+    print("""Parallel: in this implementation method, both systems work in parallel for a short period of time. This method also allows measuring output of the new system before the older system is taken offline. If the new system's functionality is not satisfactory, the company can still rely on the old system while trying to iron out any issues with the new system. This method can be expensive and taxing, as you are running both systems at the same time. This may require additional computer or network resources, additional man hours, and financial expenses to keep both running at the same time. If the two systems are drastically different, this can be extremely ineffecient for however long the changeover takes.
+    
+    Big Bang/Direct/Immediate:in this implementation method, as soon as preperations are made, one system is entirely discontinued, and the other brought online. This method is the least taxing on computer and network resources, but requires frontloading training prior to the changeover. Additionally, any data that needs to be transfered from one system to the other may need a tertiary system or external location while the changeover occurs.
+    
+    Pilot: this implementation method has a smaller portion of an organization or company change to the new system, called the pilot group or pilot site. This is fairly common in larger organizations and companies that have multiple divisions or sites. This implementation method can allow time to test the new system and iron-out issues with lower risk before it is implemented to the whole organization. Once the new system is succesful for the pilot group, changeover method for the rest of the organization can be planned. The pilot group can serve as a model when the the new system is adopted.
+    
+    Phased: this implementation method sees the company or organization convert one module or part of the system at a time over a period of time. This may mean the new system and old system may need to be similar enough for data or processes to go back and forth between the two. Training time is extended and the process overall will generally take longer as each phase of the new system is implemented seperately. These phases of implementation may be based on certain processes or functions across the whole organization, or may be based around departments or teams within the organization.
+    """)
+
+  elif(userchoice == "3"):
+    print("Related to Data Migration")
+    print("There may be complications in transfering, whether changing mediums, like going from paper files to digital ones. There may aslo be incompatibility of data types in digital formats, like data stored in pdfs to go into a csv spreadsheet format, or data stored in floats trying to be input into integer formats. Other issues may arise if the data migration process times out, fails, or encounters other errors. Data migration is the process of moving data from one location to another, one format to another, or one application to another. Generally, this is the result of introducing a new system or location for the data.")
+    print("""
+    I think my dad does something like this... I don't really talk to him that often. there is offline transfer then provider owned devices, and online transfer.""")
+
+  elif(userchoice == "4"):
+    print("""Data migration should go through a series of stages; Plan, Migrate, & Validate:
+Plan: planning process should include determining the requirement of the migration. Future environment, development, and documentation of migration plan should all be considered.
+Migrate: during this phase, the plan is communicated, all needed software and hardware is otained, installed and configured, and the process of actual transference occurs.
+Validate: in this phase, tests check that data is in the same state before and after the migration. It is recommended that tests are run pre-migration as well as post migration to have a benchmark prior to the process.""")
+
+  elif(userchoice == "5"):
+    print("""1. Poor Knowledge of Source Data
+
+This knowledge gap includes not being aware of the problems that exist in your data, such as duplicates, missing information, misspellings and erroneous data. It can be all too easy to get complacent and assume that your data can easily be configured into the parameters of a new system however the reality could mean critical failures when it comes to user acceptance. So to ensure success, you need a good understanding of the source data.
+
+2. Underestimating Data Analysis
+
+Due to constraints in computer systems, information can be hidden in obscure places because often there aren’t specific fields to hold all elements of the data or users may not be aware of the purpose of the available fields. This will result in incomplete, inaccurate and outdated information being transferred during the migration, often discovered very late in the day, even after the project has been completed. The outcome can mean not having enough time or the right resources to be able to identify and correct this data. Performing a thorough data analysis at the earliest possible occasion, usually when planning and designing your data migration can help you uncover these hidden errors.
+
+3. Lack of Integrated Processes
+
+Data migrations typically involve a disparate set of people using disparate technologies. The classic example is the use of spreadsheets to document data specifications, which is prone to human errors and cannot be easily translated when analysing data or performing data transformations. Using disparate technologies can lead to failure in the transfer of data and its design between the analysis, development, testing and implementation phases. Things can get lost in translation, resulting in increased costs and wasted time. Organisations must look to utilise a platform that successfully links the critical inputs and outputs from each of the stages to help reduce error and save time and money.
+
+4. Inability to Validate a Specification
+
+While you may well have an understanding of your source data, this will not necessarily result in a strong specification for migrating and modifying data into a target system. As this is early in the stage of the migration, critical misses can have repercussions later in the chain of activities. Validating your data transformation specifications early on with actual data, rather than just documented aspirations can increase the confidence in executing the rest of the steps.
+
+5. Failure to Validate the Implementation
+6. Late Evaluation of the Final Results.
+7. Lack of Collaboration
+8. Inappropriate use of Expertise""")
+    
+  elif(userchoice == "6"):
+    print("Related to Business M&A where I explain 7 and 8")
+    print("Essentially, it gets pretty confusing when two buisness mergers and because they use different systems, things can get pretty confusing. Merging or acquiring may seek to reduce costs by controlling more of the process, reducing competition, or other factors. Ultimately, the merger should result in a competitive advantage for the new merged organization/company. \n \n what happends when companys merge? \n \n A lot happens when a merge happen. Lots of employees are relased and some of the programs become a bit redundant. Mergers of companys are often detrimental at first but later become beneficial. When a large company buys a smaller one its called an acqusition, when they joined the stock forms into a new one. The acquistion will give up their shares. The accuried will often have to fall in line with the larger company. \n \n This is about the four main categories of mergers. /n Type one horiztional sell the same product or services and they come together. The next one is vertical, Vertical means: So where a company purches a smaller company, they will acuquire something that benefits their supply chain. Like buy a manfucature. Concnetirc is where two companies who have diffeernt products and services come together. an example is a company that sells shoes will merge with a company that sells socks. Congolmenerate, when a company will acuire a completly seperate company, to diversify more. Not really that pervalent in todays day and age.")
+
+  elif(userchoice == "7"):
+    print("""For this 9 weeks (before mid-september), one of your project grades will be system mini-study. A write up sumarizing your research and learning about the system a company or organization uses that included their tech, people, and processes.
+For the end of today, let's start doing a bit of research:
+
+Find 3 examples of systems that includes technology/human interaction.
+Amazon Wearhouse
+Krogers also funcitons like a system, with objects in a particualr space completing functions.
+Walmart pickup
+
+For each:
+- - - Find 3 resources that you can learn more about this system.
+https://aws.amazon.com/solutions/case-studies/amazon-fulfillment-aurora/
+https://www.thekrogerco.com/about-kroger/our-business/ 
+https://corporate.walmart.com/about""")
+  else: 
+    print("INVALID INPUT, please try again")
+    answer = "empty"
+    userchoice = input()
